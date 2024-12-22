a733d35e908f2ebbda5f87a945ca0f30276c3573
Dennis Muensterer <dennismuensterer@gmail.com>
2024-12-22 19:02:20 +0100

    Add vercel config to handle rewrites

--- src/services/spotify.ts
+++ src/services/spotify.ts
@@ -1,7 +1,7 @@
 import { SpotifyApi } from '@spotify/web-api-ts-sdk';
 
 const CLIENT_ID = 'cf947528ed45464e99b08b7ddb1937d3';
-const REDIRECT_URI = 'https://playlist-stats-spotify.vercel.app';
+const REDIRECT_URI = 'https://playlist-stats-spotify.vercel.app/callback';
 const SCOPES = ['playlist-read-private', 'playlist-read-collaborative'];
 
 export const spotify = SpotifyApi.withUserAuthorization(
